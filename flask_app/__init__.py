0f13fc617a1753dd0006e66ba9ce64c529279f04
Sinduda <81467922+Sinduda@users.noreply.github.com>
2022-06-24 00:15:30 +0900

    try

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import pickle
+
+# ML 모델 가져오기
+model = None
+with open('model.pkl', 'rb') as pickle_file:
+    model = pickle.load(pickle_file)
+
 
 def create_app():
     app = Flask(__name__)
@@ -6,6 +13,12 @@ def create_app():
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    @app.route('/predict')
+    def home():
+        X_test = [[90, 2002, 6]]
+        y_pred = model.predict(X_test)
+        return print('예측 가격 = ', round(y_pred[0][0]), '만원')
 
     return app
 
