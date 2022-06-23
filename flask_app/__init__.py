962e972339479f468551614c36124bf6b1080f36
Sinduda <81467922+Sinduda@users.noreply.github.com>
2022-06-24 00:30:29 +0900

    ddda

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -13,11 +13,11 @@ def create_app():
     def index():
         return render_template('index.html')
 
-    @app.route('/predict/')
-    def home():
-        X_test = [[90, 2002, 6]]
-        y_pred = model.predict(X_test)
-        return print('예측 가격 = ', round(y_pred[0][0]), '만원')
+    # @app.route('/predict/')
+    # def home():
+    #     X_test = [[90, 2002, 6]]
+    #     y_pred = model.predict(X_test)
+    #     return print('예측 가격 = ', round(y_pred[0][0]), '만원')
 
     return app
 
