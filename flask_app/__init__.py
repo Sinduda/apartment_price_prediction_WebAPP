26a191421c0cee723c99a818e1f5144eb2964137
Sinduda <81467922+Sinduda@users.noreply.github.com>
2022-06-24 10:23:40 +0900

    aed

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,23 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pickle
 
-# ML 모델 가져오기
-# model = None
-# with open('/Users/inhwanhwang/Desktop/AI_13_황인환_section3_project/model.pkl', 'rb') as pickle_file:
-#     model = pickle.load(pickle_file)
 
 def create_app():
     app = Flask(__name__)
+
+    # ML 모델 가져오기
+    model = None
+    with open('/Users/inhwanhwang/Desktop/AI_13_황인환_section3_project/model.pkl', 'rb') as pickle_file:
+        model = pickle.load(pickle_file)
 
     @app.route('/')
     def index():
         return render_template('index.html')
 
-    # @app.route('/predict/')
-    # def home():
-    #     X_test = [[90, 2002, 6]]
-    #     y_pred = model.predict(X_test)
-    #     return print('예측 가격 = ', round(y_pred[0][0]), '만원')
+    @app.route('/predict/')
+    def home():
+        X_test = [[90, 2002, 6]]
+        y_pred = model.predict(X_test)
+        return print('예측 가격 = ', round(y_pred[0][0]), '만원')
 
     return app
 
