66343afe89853e6bbccf9405d1269872bb58c1aa
Sinduda <81467922+Sinduda@users.noreply.github.com>
2022-06-24 11:13:20 +0900

    dd

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 import pickle
 
-# ML 모델 가져오기
-
-
 app = Flask(__name__)
+
+# ML 모델 가져오기
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def index():
@@ -12,7 +12,6 @@ def index():
 
 @app.route('/predict/')
 def home():
-    model = pickle.load(open('flask_app/model.pkl', 'rb'))
     X_test = [[90, 2002, 6]]
     y_pred = model.predict(X_test)
     return f"예측 가격 = {round(y_pred[0][0])} 만원"
