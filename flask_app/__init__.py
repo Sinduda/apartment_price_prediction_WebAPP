7de8930589c1ae87a287fa1a2f4ffcf8542c5d30
Sinduda <81467922+Sinduda@users.noreply.github.com>
2022-06-24 10:28:36 +0900

    dqwd

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,27 +1,22 @@
 from flask import Flask, render_template, request
 import pickle
 
+# ML 모델 가져오기
+model = None
+with open('model.pkl', 'rb') as pickle_file:
+    model = pickle.load(pickle_file)
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # ML 모델 가져오기
-    model = None
-    with open('model.pkl', 'rb') as pickle_file:
-        model = pickle.load(pickle_file)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
-    @app.route('/predict/')
-    def home():
-        X_test = [[90, 2002, 6]]
-        y_pred = model.predict(X_test)
-        return print('예측 가격 = ', round(y_pred[0][0]), '만원')
-
-    return app
+@app.route('/predict/')
+def home():
+    X_test = [[90, 2002, 6]]
+    y_pred = model.predict(X_test)
+    return print('예측 가격 = ', round(y_pred[0][0]), '만원')
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
