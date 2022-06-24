9507f5cf84b7c2314cb2c012418567df5e672ac5
Sinduda <81467922+Sinduda@users.noreply.github.com>
2022-06-24 10:36:44 +0900

    dd

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/predict/')
+@app.route('/predict/', methods = ['POST'])
 def home():
     X_test = [[90, 2002, 6]]
     y_pred = model.predict(X_test)
